5bb7a682e11a49d0c75a6cce376db7fc93b9997a
MRiDuL-ICE <abdulwahab22400@gmail.com>
2024-12-27 10:41:35 +0600

    use of function signature in ts

--- output/script.js
+++ output/script.js
@@ -5,3 +5,27 @@ const userDetails = (id, user) => {
 const sayHello = (user) => {
     console.log(`Hello ${user.age > 50 ? 'sir' : 'Mr.'} ${user.name}`);
 };
+// function signature
+let myFunc;
+myFunc = (a, b) => {
+    return (a + b);
+};
+let calculation;
+calculation = (a, b, c) => {
+    if (c === 'add') {
+        return a + b;
+    }
+    else if (c === 'substraction') {
+        return a - b;
+    }
+    else if (c === 'division') {
+        return a / b;
+    }
+    else if (c === 'multiplication') {
+        return a * b;
+    }
+    else {
+        return a % b;
+    }
+};
+console.log(calculation(10, 5, 'substraction'));
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,4 @@
+// type aliases
 type stringOrNum = number | string;
 type userType = { name: string, age: number }
 
@@ -9,3 +10,36 @@ const userDetails = (id: stringOrNum, user: userType) => {
 const sayHello = (user: userType) => {
     console.log(`Hello ${user.age > 50 ? 'sir' : 'Mr.'} ${user.name}`)
 }
+
+
+
+// function signature
+
+let myFunc: (x: number, y: number) => number;
+
+myFunc = (a: number, b: number) => {
+    return (a + b)
+}
+
+let calculation: (x: number, y: number, z: string) => number;
+
+calculation = (a: number, b: number, c: string) => {
+    if (c === 'add') {
+        return a + b
+    }
+    else if (c === 'substraction') {
+        return a - b
+    }
+    else if (c === 'division') {
+        return a / b
+    }
+    else if (c === 'multiplication') {
+        return a * b
+    }
+    else {
+        return a % b
+    }
+}
+
+
+console.log(calculation(10, 5, 'substraction'))
