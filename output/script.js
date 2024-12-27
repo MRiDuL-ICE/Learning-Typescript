0052839962895558faa9a305d3161418f3ff3363
MRiDuL-ICE <abdulwahab22400@gmail.com>
2024-12-27 09:12:14 +0600

    intro to tsc

---
+++ output/script.js
@@ -0,0 +1,7 @@
+"use strict";
+{
+    const country = "Bangladesh is in my heart";
+    const number = 10;
+    const boo = !true;
+    console.log(country, number, boo);
+}
---
+++ src/script.ts
@@ -0,0 +1,7 @@
+{
+    const country: string = "Bangladesh is in my heart"
+    const number: number = 10
+    const boo: boolean = !true
+
+    console.log(country, number, boo)
+}
